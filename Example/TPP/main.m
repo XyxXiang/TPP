ad005360fd15083bd42d37b6e252e8fdc82d053c
Lix <>
2018-06-04 01:11:26 +0800

    Initial commit

---
+++ Example/TPP/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TPP
+//
+//  Created by Lix on 06/04/2018.
+//  Copyright (c) 2018 Lix. All rights reserved.
+//
+
+@import UIKit;
+#import "LXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LXAppDelegate class]));
+    }
+}
